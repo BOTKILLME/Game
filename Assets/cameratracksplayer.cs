f7b230e4da99023c17f8d7819d39287da8fb7560
Andrew Timmis <461766@mymail.sussexdowns.ac.uk>
2018-01-30 16:14:23 +0000

    inital commit

---
+++ Assets/cameratracksplayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameratracksplayer : MonoBehaviour {
+
+    Transform player;
+
+
+	// Use this for initialization
+	void Start () {
+        GameObject player_go = gameobject.FindGameObjectWithTag("player");
+
+        if (player_go == null)
+            Debug.LogError("Couldn't Find a Object with the Tag 'Player' ");
+            return;
+        {
+
+        }
+
+        player = player_go.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
